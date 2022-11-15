e1d5c22507c441299fc0feb7aff904aad99bc39d
Crispel Clément <clement.crispel@gmail.com>
2022-11-15 17:41:15 +0100

    new hash

--- my_hash.py
+++ my_hash.py
@@ -14,4 +14,19 @@ def my_hash (message : str)-> str:
     
     hashMess += str(nbEven)
     hashMess = hashMess.replace("0","")
+    size = len(hashMess)
+    tmp = ""
+    for i in range(size):
+        tmp += str(int(hashMess[i])*size)
+        if size < 20:
+            size = len(hashMess) + len(tmp) - 1
+        else:
+            size = 20
+
+    hashMess = tmp
     return hashMess
+
+message = "2052"
+print(my_hash(message))
+message = "2052"
+print(my_hash(message))
